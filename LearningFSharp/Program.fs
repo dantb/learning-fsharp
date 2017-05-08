e5aac7de10941847bb6a3abff94a190e4a82bb14
dantb <dantb@hotmail.co.uk>
2017-05-08 19:45:26 +0100

    Initial commit

---
+++ LearningFSharp/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+module HelloSquare
+
+let square x = x * x
+let isOdd x = x % 2 <> 0
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%d square is %d" 12 (square 12)
+    0 // return an integer exit code
