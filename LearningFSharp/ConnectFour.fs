dfaa0b5a8a002539ab64018eab971bc62738f41a
dantb <dantb@hotmail.co.uk>
2017-05-08 21:42:43 +0100

    Creating connect four game to get the basics Will display in console app for now.

---
+++ LearningFSharp/ConnectFour.fs
@@ -0,0 +1,41 @@
+﻿namespace ConnectFour
+
+open System
+
+module ConnectFour =
+
+    type Node =
+        | Red
+        | Yellow
+        | Empty
+        
+    type Game =
+        { Grid : Node[,]
+          Winner : string
+        }
+
+        member this.getNode row col =
+            this.Grid.[row, col]
+
+    let game = 
+        { Grid = Array2D.init<Node> 6 7 (fun x y -> Empty) 
+          Winner = "" }
+
+    let getNode row col =
+        game.getNode row col
+
+    let getNodeString row col =
+        let nodeString =
+            match getNode row col with
+            | Empty -> "Empty"
+            | Red -> "Red"
+            | Yellow -> "Yellow"
+        nodeString
+
+    let printGame =
+        for row in 0 .. 5 do
+            for col in 0 .. 6 do
+                printf "row %i col %i has %s node" row col (getNodeString row col)
+
+ 
+
--- LearningFSharp/Program.fs
+++ LearningFSharp/Program.fs
@@ -1,12 +1,29 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
+open System
+open ConnectFour
 
-module HelloSquare
-
+//can run these in F# interactive window by either
+// isOdd (square 12);;
+//or
+// 12 |> square |> isOdd;;
 let square x = x * x
+
 let isOdd x = x % 2 <> 0
+
+let rec fibonacci n =
+    if n = 0 then 1
+    elif n = 1 then 1
+    else fibonacci (n-1) + fibonacci (n-2)
 
 [<EntryPoint>]
 let main argv = 
     printfn "%d square is %d" 12 (square 12)
+    printfn "fibonacci of %d is %d" 5 (fibonacci 5)
+
+    Console.ReadKey() |> ignore
+
+    ConnectFour.printGame |> ignore
+
+    Console.ReadKey() |> ignore
     0 // return an integer exit code
