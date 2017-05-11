a25173d8e2130c9472eb9fe53f97117f8465ebcf
dantb <dantb@hotmail.co.uk>
2017-05-11 09:15:42 +0100

    Players can taken turns with moves. Input validation also introduced

--- LearningFSharp/ConnectFour.fs
+++ LearningFSharp/ConnectFour.fs
@@ -1,27 +1,50 @@
-﻿namespace ConnectFour
+﻿ module ConnectFour
 
-open System
-
-module ConnectFour =
+    open System
 
     type Node =
         | Red
         | Yellow
         | Empty
-
+ 
     let getRed = Red
-
+ 
     let getYellow = Yellow
-        
+       
     type Game() =
-        let game = Array2D.init<Node> 6 7 (fun x y -> Empty) 
+        let game = Array2D.init<Node> 6 7 (fun x y -> Empty)
+        let winner = Array.init<Node> 1 (fun x -> Empty)
+        let currentPlayer = Array.init<Node> 1 (fun x -> Yellow)
+
+        member this.getArray =
+            game
+
+        member this.setArray (nodes:_[,]) =
+            for row = 0 to 5 do
+                for col = 0 to 6 do
+                    game.[row,col] <- nodes.[row,col]
 
         member this.getNode row col =
             game.[row, col]
-
+ 
         member this.setNode (piece:Node) row col =
             game.[row, col] <- piece
-
+ 
+        member this.getWinner =
+            winner.[0]
+ 
+        member this.setWinner node =
+            winner.[0] <- node
+ 
+        member this.getCurrentPlayer =
+            currentPlayer.[0]
+ 
+        member this.setCurrentPlayer (player:Node) =
+            currentPlayer.[0] <- player
+ 
+    let thereIsWinner (node:Node) =
+        node <> Empty
+ 
     let getNodeString (g:Game) row col =
         let nodeString =
             match g.getNode row col with
@@ -29,7 +52,7 @@ module ConnectFour =
             | Red -> "Red"
             | Yellow -> "Yellow"
         nodeString
-
+ 
     let getNodeColour (g:Game) row col =
         let nodeColour =
             match g.getNode row col with
@@ -37,21 +60,21 @@ module ConnectFour =
             | Red -> ConsoleColor.Red
             | Yellow -> ConsoleColor.Yellow
         nodeColour
-
+ 
     let updateGame (g:Game) (piece:Node) row col =
         g.setNode piece row col
         g
-
+ 
     let getGame (g:Game) = g
-
-    let decrementInt (i:int) = 
+ 
+    let decrementInt (i:int) =
         let j = i - 1
         j
-
+ 
     let plusOne (i:int) = i + 1
-
+ 
     let rec getNextEmptyRow (g:Game) stop row col =
-        if stop then 
+        if stop then
             row
         elif row = -1 then
             row + 1 //hit the bottom
@@ -60,12 +83,12 @@ module ConnectFour =
                 getNextEmptyRow g true (row + 1) col
             else
                 getNextEmptyRow g false (row - 1) col
-
+ 
     let dropPiece (g:Game) (piece:Node) col =
         let row = (getNextEmptyRow g false 5 col)
         updateGame g piece row col
-
-    let cprintf c fmt = 
+ 
+    let cprintf c fmt =
         Printf.kprintf
             (fun s ->
                 let old = System.Console.ForegroundColor
@@ -75,7 +98,7 @@ module ConnectFour =
                 finally
                   System.Console.ForegroundColor <- old)
             fmt
-
+ 
     let printGame (g:Game) =
         printf "\n"
         for row = 5 downto 0 do
@@ -84,7 +107,5 @@ module ConnectFour =
                 let string = getNodeString g row col
                 cprintf colour " O "
             printf "\n"
-
-
+        printf "\n"
  
-
--- LearningFSharp/Program.fs
+++ LearningFSharp/Program.fs
@@ -2,43 +2,75 @@
 // See the 'F# Tutorial' project for more help.
 open System
 open ConnectFour
-
+ 
 //can run these in F# interactive window by either
 // isOdd (square 12);;
 //or
 // 12 |> square |> isOdd;;
 let square x = x * x
-
+ 
 let isOdd x = x % 2 <> 0
-
+ 
 let rec fibonacci n =
     if n = 0 then 1
     elif n = 1 then 1
     else fibonacci (n-1) + fibonacci (n-2)
-
+ 
+let readLine() =
+    try
+        Console.ReadLine() |> int
+    with
+    | :? System.FormatException ->
+        -1
+ 
 [<EntryPoint>]
-let main argv = 
-    printfn "%d square is %d" 12 (square 12)
-    printfn "fibonacci of %d is %d" 5 (fibonacci 5)
-
-    Console.ReadKey() |> ignore
-
+let main argv =
+//    printfn "%d square is %d" 12 (square 12)
+//    printfn "fibonacci of %d is %d" 5 (fibonacci 5)
+ 
+    Console.ReadKey() |> ignore 
+ 
+    let invalidInput col =
+        col < 0 or col > 6
+  
+    let rec getInput invalid col =
+        if invalid then           
+            Console.Write("Enter a column between 0 and 6 inclusive in which to drop your piece: ")
+            let col = readLine()
+            getInput (invalidInput col) col
+        else
+            col
+ 
     let game = new ConnectFour.Game()
-    ConnectFour.printGame game 
-
-    Console.ReadKey() |> ignore
-
-    ConnectFour.dropPiece game ConnectFour.getRed 2
-
-    Console.ReadKey() |> ignore
-
+    //yellow goes first
+    game.setCurrentPlayer ConnectFour.getYellow
     ConnectFour.printGame game
-
-    ConnectFour.dropPiece game ConnectFour.getYellow 2
-
-    Console.ReadKey() |> ignore
-
-    ConnectFour.printGame game
-
+ 
+    let noWinner = false
+    //game loop
+    while game.getWinner = ConnectFour.Empty do
+        if game.getCurrentPlayer = ConnectFour.getRed then
+            printf "Red's turn\n"
+        else printf "Yellow's turn\n"
+        let col = getInput true -1
+ 
+        let otherPlayer (colour:ConnectFour.Node) =
+            if colour = ConnectFour.getRed then ConnectFour.getYellow
+            else ConnectFour.getRed
+ 
+        let dropPieceColour (colour:ConnectFour.Node) =
+            let g = ConnectFour.dropPiece game colour col
+            game.setArray g.getArray
+            game.setWinner g.getWinner
+            game.setCurrentPlayer (otherPlayer game.getCurrentPlayer)
+ 
+        if game.getCurrentPlayer = ConnectFour.getRed then
+            dropPieceColour ConnectFour.getRed
+        elif game.getCurrentPlayer = ConnectFour.getYellow then
+            dropPieceColour ConnectFour.getYellow
+ 
+        ConnectFour.printGame game
+ 
+ 
     Console.ReadKey() |> ignore
     0 // return an integer exit code
